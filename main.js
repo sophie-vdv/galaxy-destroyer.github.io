bec4b04db3c5efcf8a6294618eadc4eb248d37ac
sophie-vdv <vanderveken.sophie@gmail.com>
2016-10-10 17:11:35 +0100

    space setup

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 // Component to change to random color on click.
 AFRAME.registerComponent('cursor-destroy', {
     init: function () {
+        var explosion_image = document.getElementById('explosion');
         this.el.addEventListener('click', function () {
-            console.log('ok');
+            var target_position = AFRAME.utils.entity.getComponentProperty(this, 'position');
+            AFRAME.utils.entity.setComponentProperty(explosion_image, 'position', target_position);
+            this.setAttribute('visible', false);
+            explosion_image.setAttribute('visible', true);
+            explosion_image.emit('BANG');
+
+
+            console.log('ok', target_position);
             var audio = document.getElementById("audio");
             audio.play();
         });
