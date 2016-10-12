fda2d9c06dd908f0b6f1586db161b255587b3f63
sophie-vdv <vanderveken.sophie@gmail.com>
2016-10-12 18:03:10 +0100

    Add astronaut image

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
+var numPlanet = 0;
 
 AFRAME.registerComponent('cursor-destroy', {
-    init: function () {
+    init : function () {
         var explosion_image = document.getElementById('explosion');
+        var audio = document.getElementById("audio");
+
         this.el.addEventListener('click', function () {
+
+            audio.play();
+
             var target_position = AFRAME.utils.entity.getComponentProperty(this, 'position');
             AFRAME.utils.entity.setComponentProperty(explosion_image, 'position', target_position);
             this.setAttribute('visible', false);
+
             explosion_image.setAttribute('visible', true);
             explosion_image.emit('BANG');
 
+            numPlanet = numPlanet || document.querySelectorAll('.planet').length;
+            numPlanet--;
 
-            console.log('ok', target_position);
-            var audio = document.getElementById("audio");
-            audio.play();
+            document.getElementById("remaining").innerHTML = numPlanet;
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var totalPlanet = document.querySelectorAll('.planet').length;
+    document.getElementById("total").innerHTML = totalPlanet;
+
+    var timePeriodInMs = 4000;
+
+    setTimeout(function () {
+            document.getElementById("wasd").style.display = "none";
+        },
+        timePeriodInMs);
+});
+
